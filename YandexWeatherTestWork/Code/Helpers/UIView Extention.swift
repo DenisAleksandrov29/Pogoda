33c4adb49ad7bcb271b781217f80309298e0d101
Денис <57912610+DenisAleksandrov29@users.noreply.github.com>
2022-07-28 11:03:32 +0300

    Add files via upload

---
+++ YandexWeatherTestWork/Code/Helpers/UIView Extention.swift
@@ -0,0 +1,16 @@
+//
+//  UIView Extention.swift
+//  YandexWeatherTestWork
+//
+//  Created by Денис Александров on 10.03.2022.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func toAutoLayout() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
